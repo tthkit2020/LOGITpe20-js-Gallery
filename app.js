181ee9094e6d44bf437018ddefa61afa674f1932
Julia Aasmaa <my_e_10@hotmail.com>
2020-11-11 14:58:14 +0200

    complete

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const currentImage = document.querySelector('#image');
+const images = ['img/pic1.jpeg', 'img/pic2.jpg', 'img/pic3.jpg', 'img/pic4.jpg'];
+
 function changeImage(){
     console.log("Hello from script file!");
+    let rnd = Math.floor(Math.random() * images.length);
+    currentImage.src = images[rnd];
+    console.log(rnd);
+
 }
